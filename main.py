d7f7ca05f4db613140224422801a1f3bfb9e1fe8
hisert <100667448+hisert@users.noreply.github.com>
2024-05-24 15:38:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,12 @@ check_veri("veri.json")
 def degeri_oku(degisken_adi):
     try:
         with open("veri.json", "r") as dosya:
-            veri = json.load(dosya)
-            return veri.get(degisken_adi)
+            dosya_icerigi = dosya.read().strip()  # Dosyanın içeriğini oku ve başındaki ve sonundaki boşlukları temizle
+            if dosya_icerigi == "empty":
+                return None  # Dosya içeriği "empty" ise None döndür
+            else:
+                veri = json.loads(dosya_icerigi)  # Dosya içeriğini JSON olarak yükle
+                return veri.get(degisken_adi)
     except FileNotFoundError:
         print("Dosya bulunamadı.")
         return None
